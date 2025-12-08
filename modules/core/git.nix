45a5918d49a2168d361550f1a3975cc19f89a341
khalednasr <k.nasr92@gmail.com>
2025-12-08 09:58:35 +0100

    update

--- modules/core/git.nix
+++ modules/core/git.nix
@@ -6,6 +6,7 @@
       init.defaultBranch = "main";
       user.name = "${globals.gitUsername}";
       user.email = "${globals.gitEmail}";
+      push.autoSetupRemote = true;
       url = { "https://github.com/" = { insteadOf = [ "gh:" "github:" ]; }; };
     };
   };
