e6d6bfa5c7184180abde1945736ac5e00c564b53
khalednasr <k.nasr92@gmail.com>
2026-01-06 10:50:53 +0100

    update

--- hosts/boxypi/default.nix
+++ hosts/boxypi/default.nix
@@ -11,6 +11,6 @@
 
   # Initial credentials
   users.mutableUsers = true;
-  users.users.root.initialPassword = "nixos";
-  users.users."${globals.username}".initialPassword = "nixos";
+  users.users.root.initialHashedPassword = "$y$j9T$EBGJZhKc1UEufVIlCnPUx0$oJ9sUx4pYh4UjCxnf2ezrkjvMibRKT4XD7UibTIBJCA";
+  users.users."${globals.username}".initialHashedPassword = "$y$j9T$48ADycPVIa7.VrgKUoEap/$AS/EgZiDP0Z3tpJ1n2So.Zc/Feo27uLS/pcgNoFfYd6";
 }
