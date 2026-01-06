ea4528b25d2129f6050916d6f0bb7888dc2d5f28
khalednasr <k.nasr92@gmail.com>
2026-01-06 10:01:31 +0100

    update

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -40,10 +40,6 @@
     ignoreShellProgramCheck = true;
   };
 
-  users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ0U3xU6tYLgE3+jtwSxj3D0Z1rS83CaaEPvaxJf/Th3 nasrk@yoyo"
-  ];
-
   # Home manager
   imports = [ inputs.home-manager.nixosModules.home-manager ];
   home-manager = {
