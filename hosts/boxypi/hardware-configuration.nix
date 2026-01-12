e5bb68b2b5378c65d18dcfa42292cc98f7592bc0
khalednasr <k.nasr92@gmail.com>
2026-01-13 00:26:05 +0100

    update

--- hosts/boxypi/hardware-configuration.nix
+++ hosts/boxypi/hardware-configuration.nix
@@ -4,6 +4,7 @@
     raspberry-pi-5.base
     raspberry-pi-5.page-size-16k
     raspberry-pi-5.display-vc4
+    ./rpi5-config.nix
   ];
 
   fileSystems = {
---
+++ hosts/boxypi/rpi5-config.nix
@@ -0,0 +1,17 @@
+{ ... }:
+{
+  hardware.raspberry-pi.config = {
+    all = {
+      base-dt-params = {
+        pciex1 = {
+          enable = true;
+          value = "on";
+        };
+        pciex1_gen = {
+          enable = true;
+          value = "3";
+        };
+      };
+    };
+  };
+}
