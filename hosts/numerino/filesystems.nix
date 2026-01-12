687a0764437b6654d0e9d9df57a3223bae00bc77
khalednasr <k.nasr92@gmail.com>
2026-01-12 20:26:59 +0100

    update

--- hosts/numerino/filesystems.nix
+++ hosts/numerino/filesystems.nix
@@ -3,9 +3,9 @@
 {
   boot.supportedFilesystems = [ "ntfs" ];
 
-  fileSystems."/run/media/data" = {
-    device = "/dev/disk/by-uuid/2C0A43280A42EF00"; # get UUID using lsblk -f
-    options = ["nofail"];
-    fsType = "ntfs3";
-  };
+  # fileSystems."/run/media/data" = {
+  #   device = "/dev/disk/by-uuid/2C0A43280A42EF00"; # get UUID using lsblk -f
+  #   options = ["nofail"];
+  #   fsType = "ntfs3";
+  # };
 }
