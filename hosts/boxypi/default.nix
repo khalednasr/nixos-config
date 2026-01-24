93a1ab037d7066b99783ac678a2e2ae92d263458
khalednasr <k.nasr92@gmail.com>
2026-01-24 11:38:25 +0100

    update

--- hosts/boxypi/default.nix
+++ hosts/boxypi/default.nix
@@ -16,6 +16,9 @@
   users.users.root.initialHashedPassword = "$y$j9T$EBGJZhKc1UEufVIlCnPUx0$oJ9sUx4pYh4UjCxnf2ezrkjvMibRKT4XD7UibTIBJCA";
   users.users."${globals.username}".initialHashedPassword = "$y$j9T$48ADycPVIa7.VrgKUoEap/$AS/EgZiDP0Z3tpJ1n2So.Zc/Feo27uLS/pcgNoFfYd6";
 
+  # Auto login
+  services.getty.autologinUser = "${globals.username}";
+
   # Home manager modules
   home-manager.users.${globals.username}.imports = [
   ];
